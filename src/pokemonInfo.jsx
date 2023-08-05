e0e905f626c01c98b61fa0c34e59dd0d219e5bb8
Netdreion <128006926+Netdreion@users.noreply.github.com>
2023-08-04 22:04:39 -0400

    debugging tools cleared

--- src/pokemonInfo.jsx
+++ src/pokemonInfo.jsx
@@ -10,12 +10,9 @@ const PokemonInfo = () => {
     try {
       console.log(url);
       const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error("network response was not ok");
-      }
+
       const data = await response.json();
 
-      console.log("API Response:", data);
       const { name, sprites } = data;
       setPoke({
         name,
@@ -32,7 +29,7 @@ const PokemonInfo = () => {
         value={pokeName}
         onChange={(event) => setPokeName(event.target.value)}
       />
-      <button onClick={() => fetchData()}>Fetch Pokemon</button>
+      <button onClick={() => fetchData(pokeName)}>Fetch Pokemon</button>
       {poke.name && (
         <div>
           <h2>{poke.name}</h2>
