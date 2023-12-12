d019e91df955b55a6441f58ca3386da4b6d14944
Netdreion <128006926+Netdreion@users.noreply.github.com>
2023-12-11 22:57:31 -0500

    revised

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,69 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
-import PokemonInfo from "./pokemonInfo.jsx";
+
 function App() {
+  const [pokeData, setPokeData] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+  const [filteredData, setFilteredData] = useState(null);
+
+  const url = "https://pokeapi.co/api/v2/pokemon/";
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url + "pikachu");
+        const receivedData = await response.json();
+
+        setPokeData(receivedData);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const handlePokeData = (e) => {
+    setSearchValue(e.target.value);
+  };
+
+  const handleClick = async () => {
+    try {
+      const response = await fetch(url + searchValue.toLowerCase());
+
+      if (!response.ok) {
+        console.error(`Failed to fetch Pokemon data: ${response.status}`);
+        setFilteredData(null);
+        return;
+      }
+
+      const pokemonData = await response.json();
+      setFilteredData(pokemonData);
+    } catch (error) {
+      console.error(error);
+      setFilteredData(null);
+    }
+  };
+
   return (
-    <>
-      <PokemonInfo />
-    </>
+    <div className="App">
+      <input
+        type="text"
+        placeholder="Search Pokemon"
+        value={searchValue}
+        onChange={handlePokeData}
+      />
+      <button onClick={handleClick}>Search</button>
+
+      {filteredData && (
+        <div>
+          <p>Name: {filteredData.name}</p>
+          <img
+            src={filteredData.sprites.front_default}
+            alt={`${filteredData.name} sprite`}
+          />
+        </div>
+      )}
+    </div>
   );
 }
 
